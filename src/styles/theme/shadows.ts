5c5c876e8d529bc7d0f88f004bddde979a1a228c
Jiyu215 <jinyu010512@gmail.com>
2026-03-26 19:16:06 +0900

    chore: 스타일드 컴포넌트 삭제 및 tailwind css로 전체 변경

--- src/styles/theme/shadows.ts
+++
@@ -1,7 +0,0 @@
-export const shadows = {
-  xs: '0 1px 2px rgba(0,0,0,0.05)',
-  sm: '0 1px 3px rgba(0,0,0,0.1)',
-  md: '0 4px 6px rgba(0,0,0,0.1)',
-  lg: '0 10px 15px rgba(0,0,0,0.15)',
-  xl: '0 20px 25px rgba(0,0,0,0.2)',
-};
